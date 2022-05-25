afdafa329b2a56ddad388e74bc67d8a51bf8fd51
checkiamsiam <issiam02415@gmail.com>
2022-05-25 19:59:42 +0600

    auth

--- index.js
+++ index.js
@@ -4,6 +4,7 @@ const { MongoClient, ServerApiVersion } = require('mongodb');
 const app = express()
 const port = process.env.PORT || 5000;
 require('dotenv').config()
+const jwt = require('jsonwebtoken');
 
 app.use(cors())
 app.use(express.json())
@@ -12,11 +13,28 @@ const uri = `mongodb+srv://${process.env.DB_USER}:${process.env.DB_PASSWORD}@clu
 const client = new MongoClient(uri, { useNewUrlParser: true, useUnifiedTopology: true, serverApi: ServerApiVersion.v1 });
 
 
+function verifyJWT(req, res, next) {
+  const authHeader = req.headers.authorization;
+  if (!authHeader) {
+    return res.status(401).send({ message: 'UnAuthorized access' });
+  }
+  const token = authHeader.split(' ')[1];
+  jwt.verify(token, process.env.TOKEN_SECRET, function (err, decoded) {
+    if (err) {
+      return res.status(403).send({ message: 'Forbidden access' })
+    }
+    req.decoded = decoded;
+    next();
+  });
+}
+
+
 async function run() {
   try {
     await client.connect();
     const reviewCollection = await client.db("Bycycle-Soul-DB").collection("reviews")
     const partsCollection = await client.db("Bycycle-Soul-DB").collection("Parts")
+    const userCollection =  await client.db('Bycycle-Soul-DB').collection('users');
 
 
     app.get('/reviews', async (req, res) => {
@@ -32,6 +50,37 @@ async function run() {
       const cursor = await partsCollection.find(query)
       const results = await cursor.toArray();
       res.send(results);
+    })
+
+    app.get('/users', verifyJWT, async (req, res) => {
+      const users = await userCollection.find().toArray()
+      res.send(users)
+    })
+
+    
+    app.put('/users/:email', verifyJWT, async (req, res) => {
+      const email = await req.params.email
+      const filter = { email };
+      const options = { upsert: true };
+      const updateDoc = {
+        $set: {
+          role: 'admin'
+        },
+      };
+      const result = await userCollection.updateOne(filter, updateDoc, options);
+      res.send(result)
+    })
+    
+    app.post('/users', async (req, res) => {
+      const postItem = await req.body;
+      const query = { email: postItem.email };
+      const alreadyUser = await userCollection.findOne(query);
+      var token = jwt.sign(query, process.env.TOKEN_SECRET, { expiresIn: '1h' });
+      if (alreadyUser) {
+        return res.send({ message: 'already added', accessToken: token })
+      }
+      const result = await userCollection.insertOne({ email: postItem.email, role: 'member' });
+      res.send({ accessToken: token })
     })
 
   } finally {
