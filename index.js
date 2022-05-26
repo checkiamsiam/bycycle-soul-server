fcc75dc33ffa2e92c67908752aa625b1fa2c9ea1
checkiamsiam <issiam02415@gmail.com>
2022-05-26 22:05:01 +0600

    delete

--- index.js
+++ index.js
@@ -1,6 +1,6 @@
 const express = require('express')
 const cors = require('cors')
-const { MongoClient, ServerApiVersion } = require('mongodb');
+const { MongoClient, ServerApiVersion, ObjectId } = require('mongodb');
 const app = express()
 const port = process.env.PORT || 5000;
 require('dotenv').config()
@@ -125,6 +125,14 @@ async function run() {
     app.get('/orders' , async (req, res) => {
       const result = await orderCollection.find().toArray()
       res.send(result)
+    })
+
+    app.delete('/orders/:id', async (req, res) => {
+      const id = req.params.id;
+      const deleteItem = { _id: ObjectId(id) };
+      const result = await orderCollection.deleteOne(deleteItem);
+      res.send(result)
+
     })
 
   } finally {
